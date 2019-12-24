317c07e32d187d6bd45f653aefa9a47790915b59
kashif <kashifriazgh@gmail.com>
2019-12-24 11:14:17 -0800

    initial change

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
 import React from "react"
+import "./styles/header.scss"
 
-export default () => <div><h1>Hello Only!</h1></div>
+export default () => <div>
+    <h1>Hello Only! Hello</h1>
+    <form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+    </div>
